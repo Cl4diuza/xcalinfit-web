52e7baf9fc5a9256a417d8df66c9aec6b904da8b
Tanapat Opal <tanapat.c@aware.co.th>
2019-04-10 17:07:19 +0700

    update form calculate and style

--- src/app/calintake/calintake.component.ts
+++ src/app/calintake/calintake.component.ts
@@ -8,6 +8,17 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CalintakeComponent implements OnInit {
   calForm: FormGroup;
+  totalCal: any;
+  proteinIntake: any;
+  carbIntake: any;
+  fatIntake: any;
+  proteinCal: any;
+  fatCal: any;
+  carbCal: any;
+  bmr: any;
+  goalCal: any;
+
+  isCalculated: any = false;
 
   constructor() {}
 
@@ -15,9 +26,50 @@ export class CalintakeComponent implements OnInit {
     this.initForm();
   }
 
-  onSubmit() {
-    console.log(this.calForm);
+  onCalculate() {
+    const goal = this.calForm.controls.goal.value;
+    const sex = this.calForm.controls.sex.value;
+    const weight = this.calForm.controls.weight.value;
+    const height = this.calForm.controls.height.value;
+    const age = this.calForm.controls.age.value;
+    const exercise = this.calForm.controls.exercise.value;
+
+    this.calculateBmr(sex, weight, height, age);
+    this.totalCal = exercise * this.bmr;
+    this.calculateGoalCal(this.totalCal, goal);
+    this.calculateNutrient();
+    this.isCalculated = true;
+    window.scrollTo(0, 0);
   }
+
+  calculateBmr(sex, weight, height, age) {
+    if (sex === 'male') {
+      this.bmr = 66 + 13.7 * weight + 5 * height - 6.8 * age;
+    } else {
+      this.bmr = 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
+    }
+  }
+
+  calculateGoalCal(totalCal, goal) {
+    if (goal === 'diet') {
+      this.goalCal = totalCal * 0.8;
+    } else if (goal === 'stable') {
+      this.goalCal = totalCal;
+    } else {
+      this.goalCal = totalCal * 1.2;
+    }
+  }
+
+  calculateNutrient() {
+    this.carbCal = this.goalCal * 0.4;
+    this.proteinCal = this.goalCal * 0.3;
+    this.fatCal = this.goalCal * 0.3;
+
+    this.carbIntake = this.carbCal / 4;
+    this.proteinIntake = this.proteinCal / 4;
+    this.fatIntake = this.fatCal / 9;
+  }
+
   private initForm() {
     this.calForm = new FormGroup({
       goal: new FormControl(),
